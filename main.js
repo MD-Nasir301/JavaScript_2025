135d10efffa3bd074dcd5f949eda838407726172
nasir21b@gmail.com <nasir21b@gmail.com>
2025-12-27 18:34:18 +0600

    learn array method reduce

--- main.js
+++ main.js
@@ -68,40 +68,34 @@ let ne = arr1.map((a) => a * 2);
 
 console.log(ne);
 
-
 let price = [
-  1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
-  11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
-  21, 22, 23, 24, 25, 26, 27, 28, 29, 30,
-  31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
-  41, 42, 43, 44, 45, 46, 47, 48, 49, 50,
-  51, 52, 53, 54, 55, 56, 57, 58, 59, 60,
-  61, 62, 63, 64, 65, 66, 67, 68, 69, 70,
-  71, 72, 73, 74, 75, 76, 77, 78, 79, 80,
-  81, 82, 83, 84, 85, 86, 87, 88, 89, 90,
-  91, 92, 93, 94, 95, 96, 97, 98, 99, 100
+  1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22,
+  23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41,
+  42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60,
+  61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79,
+  80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98,
+  99, 100,
 ];
 
-let userInput = document.getElementById("myRange")
-let rangeDisplay = document.getElementById("rangeValue")
-let arrDisplay = document.getElementById("arr")
+// let userInput = document.getElementById("myRange")
+// let rangeDisplay = document.getElementById("rangeValue")
+// let arrDisplay = document.getElementById("arr")
 
+// userInput.addEventListener("input", () => {
 
-userInput.addEventListener("input", () => {
+// rangeDisplay.textContent = userInput.value;
+// arrDisplay.textContent = price.filter(a => a<=userInput.value)
 
-rangeDisplay.textContent = userInput.value;
-arrDisplay.textContent = price.filter(a => a<=userInput.value)
-  
-} )
+// } )
 
-console.log(userInput.value);
+let cart = [5, 20, 10, 10, 25, 50, 30, 5, 30];
+let discount = document.getElementById("discount");
+let totalPrice = document.getElementById("totalPrice");
+let finalPrice = document.getElementById("finalPrice");
 
-
-
-
-
-
-
-
-
-
+discount.addEventListener("input", () => {
+  let result = cart.reduce((a, cr) => a + cr);
+  totalPrice.textContent = result;
+  let finalResutl = result - (result * discount.value) / 100;
+  finalPrice.textContent = finalResutl;
+});
